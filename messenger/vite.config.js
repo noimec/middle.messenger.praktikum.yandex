34081f4503cc9197a35e36e0214a3ad8431baf36
Stepan <97578379+ingerstep@users.noreply.github.com>
2024-02-01 06:59:12 +0700

    init messenger project
    Vite, Express, Sass, Handlebars-plugin.

---
+++ messenger/vite.config.js
@@ -0,0 +1,18 @@
+import {resolve} from 'path'
+import { defineConfig } from 'vite'
+import handlebars from 'vite-plugin-handlebars';
+
+export default defineConfig({
+    root: resolve(__dirname, 'static'),
+    build: {
+        outDir: resolve(__dirname, 'dist')
+    },
+    plugins: [handlebars(
+        {
+            partialDirectory: resolve(__dirname, 'src/partials'),
+            context: {
+                username: 'Test'
+            }
+        }
+    )],
+})
