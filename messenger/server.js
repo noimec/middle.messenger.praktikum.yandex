823537761dbeaf62abe84ae2d13e4da92c399eb4
Stepan <97578379+ingerstep@users.noreply.github.com>
2024-02-02 03:59:06 +0700

    Add pages + changed READ.me

--- messenger/server.js
+++ messenger/server.js
@@ -10,14 +10,26 @@ app.get('/', function(_req, res) {
     res.sendFile(resolve(__dirname, 'dist/index.html'))
 })
 
+app.get('/404', function(_req, res) {
+    res.sendFile(resolve(__dirname, 'dist/404.html'))
+})
+
+app.get('/500', function(_req, res) {
+    res.sendFile(resolve(__dirname, 'dist/500.html'))
+})
+
+app.get('/chat', function(_req, res) {
+    res.sendFile(resolve(__dirname, 'dist/chat.html'))
+})
+
+app.get('/sign-in', function(_req, res) {
+    res.sendFile(resolve(__dirname, 'dist/sign-in.html'))
+})
+
 app.get('/profile', function(_req, res) {
     res.sendFile(resolve(__dirname, 'dist/profile.html'))
 })
 
-app.get('/prof', function(_req, res) {
-    res.sendFile(resolve(__dirname, 'dist/prof.html'))
-})
-
 app.listen(PORT, function(){
-    console.log(`App Messenger started on port ${PORT}`)
+    console.log(`\nApp Messenger started on port ${PORT} \nlocal - http://localhost:3000/`)
 })
