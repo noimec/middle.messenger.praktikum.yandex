0a12a7009263ed3a7447df6294c71c18af7fa23f
Stepan <97578379+ingerstep@users.noreply.github.com>
2024-02-01 09:04:04 +0700

    create multipages compiling + express server routes

--- messenger/server.js
+++ messenger/server.js
@@ -1,9 +1,22 @@
 import express from 'express';
+import {resolve} from 'path';
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('./static'));
+app.use(express.static('./dist'));
+
+app.get('/', function(_req, res) {
+    res.sendFile(resolve(__dirname, 'dist/index.html'))
+})
+
+app.get('/profile', function(_req, res) {
+    res.sendFile(resolve(__dirname, 'dist/profile.html'))
+})
+
+app.get('/prof', function(_req, res) {
+    res.sendFile(resolve(__dirname, 'dist/prof.html'))
+})
 
 app.listen(PORT, function(){
     console.log(`App Messenger started on port ${PORT}`)
